c47869b883c2bb3bbca2a75613fdf30e2d602ae0
John Ray <john@trychec.com>
2019-08-07 17:32:19 -0400

    update src/index.js: remove extra comments

--- src/index.js
+++ src/index.js
@@ -4,12 +4,8 @@ import Footer from './components/Footer.js';
 import Header from './components/Header.js';
 
 // TODO: create util function to DRY up customElementsRegistry.define use
-// register custom element 'app-container', with it's class AppContainer object defining
-// it's behavior
+
+// register custom elements
 customElements.define("app-container", App, { extends: 'div' });
-
-// register x-footer;Footer
 customElements.define("x-footer", Footer);
-
-// register x-header;Header
 customElements.define("x-header", Header);
