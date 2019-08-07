1cb0bddaabdb6b8bf84688ebf2cfc4fc6afe89c5
John Ray <john@trychec.com>
2019-08-06 23:44:45 -0400

    update src/index.js: import App, Header class. register app-container, x-footer, and x-header.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import './styles/application.scss';
-import AppContainer from './components/App.js';
+import App from './components/App.js';
 import Footer from './components/Footer.js';
+import Header from './components/Header.js';
 
 // TODO: create util function to DRY up customElementsRegistry.define use
 // register custom element 'app-container', with it's class AppContainer object defining
 // it's behavior
-customElements.define("app-container", AppContainer);
+customElements.define("app-container", App, { extends: 'div' });
 
 // register x-footer;Footer
-customElements.define("x-footer", Footer)
+customElements.define("x-footer", Footer);
+
+// register x-header;Header
+customElements.define("x-header", Header);
