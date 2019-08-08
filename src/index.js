b7f5d06330e618157670446f62a5ac7c62504255
John Ray <john@trychec.com>
2019-08-08 18:00:11 -0400

    import LandingPage and register it

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import './styles/application.scss';
 import App from './components/App.js';
 import Footer from './components/Footer.js';
 import Header from './components/Header.js';
-
+import LandingPage from './components/LandingPage.js';
 // TODO: create util function to DRY up customElementsRegistry.define use
 
 // register custom elements
 customElements.define("app-container", App, { extends: 'div' });
 customElements.define("x-footer", Footer);
 customElements.define("x-header", Header);
+customElements.define("landing-page", LandingPage);
