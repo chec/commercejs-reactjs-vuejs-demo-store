07ecbe292372ceea4a3ecc10ffc595db3bf60442
John Ray <john@trychec.com>
2019-08-10 00:12:45 -0400

    extend from withComponentState to have state functionality in header, also implement handleEvent and add eventListener to root

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,36 @@
 import setUpShadowAndRender from '../utils/setUpShadowAndRender'
+import WithComponentState from '../utils/withComponentState'
 
 // svg
 import logo from '../assets/logo.svg';
 import cartIcon from '../assets/cart-icon.svg';
 import pairShoes from '../assets/pair-shoes-small.png';
 
-class Header extends HTMLElement {
+
+class Header extends WithComponentState() {
   constructor() {
     super();
   }
 
   connectedCallback() {
     setUpShadowAndRender.call(this, undefined, true);
+    this.addEventListener('click', this) // add addEventListener to root to avoid inline scripting
   }
+
+  handleEvent(e) {
+    console.log('The event is:', e)
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(name, oldValue, newValue);
+    if (oldValue !== newValue) {
+      this[name] = newValue // update all changed value, if this isn't done then setter this.componentState connected to observed attribute componentState won't get updated
+    }
+  }
+
+  static get observedAttributes() {
+    return ['componentState'];
+  } // must observe componentState in order to receive updates
 
   render() {
     return `
@@ -22,11 +40,13 @@ class Header extends HTMLElement {
         </div>
         <div class="flex">
           <div class="productFragmentContainer mw5 mr2">
-            <button class="absolute right-0 medium-text f7 cherry bg-blossom ttu rotate-270 pv2 ph2 bg-white outline-0 dim pointer mr5">
+            <button
+              name="pre-order-button"
+              class="absolute right-0 medium-text f7 cherry bg-blossom ttu rotate-270 pv2 ph2 bg-white outline-0 dim pointer mr5">
               pre-order now
             </button>
             <div class="w-100">
-              <img src=${pairShoes} width="100" height="auto" />
+              ${ this.state.hideShoe ? '' : `<img src=${pairShoes} width="100" height="auto" />` }
             </div>
           </div>
           <div class="cartIconContainer pointer">
