2fe06de2b3c537f949e856e477a3ca7ed10bedf1
John Ray <john@trychec.com>
2019-08-10 16:02:05 -0400

    import productdetail component at entry point and register custom element

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './styles/application.scss';
-import App from './components/App.js';
-import Footer from './components/Footer.js';
-import Header from './components/Header.js';
-import LandingPage from './components/LandingPage.js';
+import App from './components/App';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import LandingPage from './components/LandingPage';
+import ProductDetail from './components/ProductDetail';
+
 // TODO: create util function to DRY up customElementsRegistry.define use
 
 // register custom elements
@@ -10,3 +12,4 @@ customElements.define("app-container", App);
 customElements.define("x-footer", Footer);
 customElements.define("x-header", Header);
 customElements.define("landing-page", LandingPage);
+customElements.define("product-detail", ProductDetail;
