55932c47bdf2c8e8ab32d54191e79dd4ad94eb4d
John Ray <john@trychec.com>
2019-08-09 21:12:25 -0400

    implement mixin to add state functionality to htmlelement class

---
+++ src/utils/setUpComponentState.js
@@ -0,0 +1,12 @@
+export default function (SuperClass) {
+  return class MixedComponent extends SuperClass {
+    get componentState() {
+      return this.state;
+    }
+    set componentState(value) {
+      console.log('*** Updating State ***')
+      this.state = value
+      this.innerHTML = this.render()
+    }
+  }
+}
