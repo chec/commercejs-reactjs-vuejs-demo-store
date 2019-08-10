4703f5a4922739df512ef44b938a7e012d9fdbf3
John Ray <john@trychec.com>
2019-08-10 05:01:27 -0400

    remove incorrect use of JSON.parse method use JSON.stringify instead to stringify props into template literal strings, also retrieve cart when ready.

--- src/components/App.js
+++ src/components/App.js
@@ -11,16 +11,31 @@ class App extends WithComponentState() {
       checkout: null,
       order: null
     }
+    // here we construct a new Commercejs client using the Commerce class from the window
+    // and assign it to an instance variable named this.commerce
     this.commerce = new window.Commerce(process.env.COMMERCEJS_PUBLIC_KEY, (process.env.NODE_ENV === 'development') ? true : false);
   }
 
   connectedCallback(){
+    if (this.commerce !== undefined && typeof this.commerce !== 'undefined') {
+      window.addEventListener("Commercejs.Cart.Ready", function () {
+        // invoke commerce cart method to retrieve cart in session
+        this.commerce.Cart.retrieve((cart) => {
+          if (!cart.error) {
+            this.setState({
+              ...this.state,
+              cart: cart
+            })
+          }
+        });
+      }.bind(this))
+    }
     setUpShadowAndRender.call(this, undefined, true)
   }
 
   render() {
     return `
-      <x-header cart="${JSON.parse(this.state.cart)}"></x-header>
+      <x-header cart="${JSON.stringify(this.state.cart)}"></x-header>
       <main id="main" class="flex">
         <landing-page class="flex flex-grow-1 items-center"></landing-page>
       </main>
