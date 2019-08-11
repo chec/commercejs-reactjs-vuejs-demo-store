c3e5438a888e437c0091212e7c99a3090982354e
John Ray <john@trychec.com>
2019-08-11 00:47:35 -0400

    implement button for product detail page and update product detail

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,7 +1,21 @@
 import setUpShadowAndRender from '../utils/setUpShadowAndRender'
 
 // assets
-import imagePlaceholder from '../assets/image-placeholder.png';
+import imagePlaceholder from '../assets/image-placeholder-no-shadow.png';
+
+function renderButton(props) {
+  return `
+    <div>
+      <p class="medium-text ttu gray f6 tracked-mega-1 pb2">
+        price
+      </p>
+      <p class="large-title-text f1 fw9">
+        $100.00 USD
+      </p>
+    </div>
+  `;
+}
+
 
 class ProductDetail extends HTMLElement {
   constructor() {
@@ -15,14 +29,14 @@ class ProductDetail extends HTMLElement {
       <div class="productDetail w-100 pb5">
         <div class="mw8 center ph2">
           <div class="cf flex flex-row items-center">
-            <div class="fl flex-grow-1 items-center justify-center mw6 mt5">
+            <div class="fl flex-grow-1 items-center justify-center mw6 mt6">
               <p class="large-title-text dark-gray w-100">
                 white shoes
               </p>
               <p class="medium-body-text gray w-90">
                 lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
               </p>
-              <button class="button button__add-to-cart white ttu bg-dark-gray tracked w-80 mv3">
+              <button class="button button__add-to-cart white ttu bg-dark-gray tracked-mega-1 w-80 mv3">
                 add to cart
               </button>
             </div>
@@ -30,6 +44,9 @@ class ProductDetail extends HTMLElement {
               <img src=${imagePlaceholder} width="100%" height="auto" />
             </div>
           </div>
+        </div>
+        <div class="productDetail__info-container mw8 center flex flex-row flex-grow-1 pb4">
+          ${renderButton()}
         </div>
       </div>
     `
