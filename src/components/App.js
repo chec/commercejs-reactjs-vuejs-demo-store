a56949e06ffcae3c962ae8f68cf3e990ea62d39f
John Ray <john@trychec.com>
2019-08-11 22:28:22 -0400

    add push state util function to window for updating path, update app-container to render components on respective route

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import setUpShadowAndRender from '../utils/setUpShadowAndRender'
 import WithComponentState from '../utils/withComponentState';
 
+window.pushStateAndTriggerPopStateEvent = (pathname) => {
+  history.pushState({}, null, pathname);
+  const popStateEvent = new PopStateEvent('popstate', {});
+  window.dispatchEvent(popStateEvent);
+}
+
 class App extends WithComponentState() {
 
   constructor() {
@@ -9,7 +15,8 @@ class App extends WithComponentState() {
       products: [],
       cart: null,
       checkout: null,
-      order: null
+      order: null,
+      pathname: window.location.pathname
     }
     // here we construct a new Commercejs client using the Commerce class from the window
     // and assign it to an instance variable named this.commerce
@@ -44,16 +51,52 @@ class App extends WithComponentState() {
       }.bind(this))
     }
     setUpShadowAndRender.call(this, undefined, true)
+    this.addEventListener('add-to-cart', this.addProductToCart.bind(this))
+    window.onpopstate = () => {
+      this.setState({
+        ...this.state,
+        pathname: window.location.pathname
+      })
+    } // listen to history state events
   }
+
+  static get observedAttributes() {
+    return ['componentState'];
+  } // must observe componentState in order to receive updates
+
+  // adds product to cart by invoking Commerce.js's Cart method 'Cart.add'
+  addProductToCart(e) {
+    alert(`the event is ${JSON.stringify(e.detail)}`)
+    const productId = e.detail.productId;
+    this.commerce.Cart.add({
+      id: productId,
+    }, (resp) => {
+      // if successful update Cart
+      if (!resp.error) {
+        this.setState({
+          ...this.state,
+          cart: resp.cart
+        })
+      }
+    });
+  }
+
 
   render() {
     return `
-      <x-header cart="${JSON.stringify(this.state.cart)}"></x-header>
+      <x-header cart="${escape(JSON.stringify(this.state.cart))}"></x-header>
       <main id="main" class="flex">
-        ${true ? `<landing-page class="flex flex-grow-1 items-center bg-black w-"></landing-page>` : ''}
-        <product-detail></product-detail>
+        ${(this.state.pathname === "/") ? `<landing-page class="flex flex-grow-1 items-center bg-black w-"></landing-page>` : ''}
+        ${(this.state.pathname === "/white-shoe") ? `
+          <product-detail
+            class="flex flex-grow-1"
+            product="${escape(JSON.stringify(this.state.products.length ? this.state.products[0] : null))}"
+          ></product-detail>
+          `:
+          ''
+        }
       </main>
-      <x-footer></x-footer>
+      <x-footer class="flex bg-black-90"></x-footer>
     `;
   }
 }
