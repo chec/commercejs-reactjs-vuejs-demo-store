0b2a966ae0a35b1c84e45fbba19c8f1735539c04
John Ray <john@trychec.com>
2019-08-12 09:22:07 -0400

    cartcheckout

---
+++ src/components/CartCheckout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CartCheckout extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+      
+      </div>
+    )
+  }
+}
