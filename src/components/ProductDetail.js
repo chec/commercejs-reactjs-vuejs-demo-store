26da9a68f02f120830ade4862675f0ed51f399a4
John Ray <john@trychec.com>
2019-08-16 03:03:37 -0400

    fix tachyons to make media query work, add !optional flag to @extend classes. make product detail component responsive

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -7,7 +7,7 @@ function Label(props) {
       <p className="medium-text ttu gray f6 tracked-mega-1 pb2">
         {props.labelTitle || '----'}
       </p>
-      <p className="large-title-text f1 fw9 ttu pl3">
+      <p className="large-title-text f2 f1-m fw9 ttu pl3">
         {props.body || (props.placeholder || '----')}
       </p>
     </div>
@@ -59,8 +59,8 @@ class ProductDetail extends Component {
     return (
       <div className="productDetail w-100 pb5 ph4">
         <div className="mw8 center ph2">
-          <div className="cf flex flex-column flex-row-m items-center">
-            <div className="fl flex flex-column flex-grow-1 items-center justify-center mw6 mt6-m order-0 order-1">
+          <div className="cf flex flex-column flex-row-l items-center">
+            <div className="fl flex flex-column flex-grow-1 items-center justify-center mw6 mt6-l order-1 order-0-l">
               <p className="large-title-text dark-gray w-100 ttl tc">
                 {product.name}
               </p>
@@ -75,7 +75,7 @@ class ProductDetail extends Component {
                 add to cart
               </button>
             </div>
-            <div className="fl w-100 w-50-m self-start relative">
+            <div className="fl w-80 w-50-l self-start-l relative">
               <img src={product.media.source} width="100%" height="auto" />
               <div className="absolute absolute--fill flex justify-end items-end ml4">
                 <div className="rotate-lift pr2">
