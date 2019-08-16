3452927f3a39030f5bea9189e28fb7ba4fd27d74
John Ray <john@trychec.com>
2019-08-16 12:50:31 -0400

    make more responsive, create class for outlining inputs related to errors

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,7 +5,7 @@ import shoesHero from '../assets/shoes-hero.png';
 
 function LandingPage(props) {
   return (
-    <div className="flex flex-grow-1 items-center bg-black pb5">
+    <div className="flex flex-grow-1 items-center bg-black pv5 pv4-m pv3-l">
       <div className="relative w-100 mw9 center ph2">
         <div className="w-90 o-40 center pv2">
           <LettersHeroBg />
