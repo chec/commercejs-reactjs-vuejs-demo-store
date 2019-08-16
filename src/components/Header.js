a4ca08b34f79782b80cee9d45706a3cbdd3b316a
John Ray <john@trychec.com>
2019-08-16 03:26:56 -0400

    make header more responsive, make cartcheckout responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,9 @@ function Header(props) {
   const currentFillColor = isThankYouPathname ? 'fill-cherry' : 'fill-near-white';
   const currentColor = isThankYouPathname ? 'cherry' : 'white';
   return (
-    <header className="absolute w-100 ph2 ph5-l pv3 pv5-l flex flex-row justify-between items-center mt3 z-1">
+    <header className="absolute w-100 ph2 ph5-l pv3 pv5-ns flex flex-row justify-between items-center items-start-ns mt3 z-1">
       <Link to="/">
-        <div className={`logoContainer dim pointer ${currentFillColor}`}>
+        <div className={`logoContainer dim pointer w-70 center w-85-l ${currentFillColor}`}>
           <Logo />
         </div>
       </Link>
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -75,7 +75,7 @@ class ProductDetail extends Component {
                 add to cart
               </button>
             </div>
-            <div className="fl w-80 w-50-l self-start-l relative">
+            <div className="fl w-90 w-50-l self-start-l relative">
               <img src={product.media.source} width="100%" height="auto" />
               <div className="absolute absolute--fill flex justify-end items-end ml4">
                 <div className="rotate-lift pr2">
@@ -90,7 +90,7 @@ class ProductDetail extends Component {
             </div>
           </div>
         </div>
-        <div className="productDetail__info-container mw8 justify-start flex flex-row flex-grow-1 flex-wrap pb4 mt4">
+        <div className="productDetail__info-container center mw8 justify-start flex flex-row flex-grow-1 flex-wrap pb4 mt4">
           <Label
             labelTitle='price'
             body='$100.00 USD'
