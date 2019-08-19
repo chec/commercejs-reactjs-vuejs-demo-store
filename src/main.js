b9be498c0ddbd3097d12bc05b349a78e0e32e4d7
John Ray <john@trychec.com>
2019-08-18 21:12:57 -0400

    install vue-router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
+Vue.use(VueRouter);
 const commerce = new window.Commerce(process.env.VUE_APP_COMMERCEJS_PUBLIC_KEY, (process.env.NODE_ENV === 'development') ? true : false);
 Vue.config.productionTip = false
 
