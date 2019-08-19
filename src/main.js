06cfd3e3006029857f5927c2c2c6b644caa88e8f
John Ray <john@trychec.com>
2019-08-18 21:01:20 -0400

    pass commercejs client as prop to App component

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+const commerce = new window.Commerce(process.env.VUE_APP_COMMERCEJS_PUBLIC_KEY, (process.env.NODE_ENV === 'development') ? true : false);
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App, { props: { commerce } }),
 }).$mount('#app')
