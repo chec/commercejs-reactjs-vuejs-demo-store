cb6bd40f24e7f612dccce98e621a8b70958c39d7
John Ray <john@trychec.com>
2019-08-26 22:04:19 -0400

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './styles/application.scss';
-import App from './App';
-
-const commerce = new window.Commerce(process.env.REACT_APP_COMMERCEJS_PUBLIC_KEY, (process.env.NODE_ENV === 'development') ? true : false);
-ReactDOM.render(<Router><App commerce={commerce}/></Router>, document.getElementById('root'));
