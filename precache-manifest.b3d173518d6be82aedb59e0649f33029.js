908e594dc21dc5af068279969827de4adb5eef9d
John Ray <john@trychec.com>
2019-08-26 23:28:59 -0400

    Updates

---
+++ precache-manifest.b3d173518d6be82aedb59e0649f33029.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1b978ade1010765f6d5ffa72cd3c69",
+    "url": "/example.checkout.v2/index.html"
+  },
+  {
+    "revision": "54c6f82ce6bb59fa32d9",
+    "url": "/example.checkout.v2/static/css/main.c623d524.chunk.css"
+  },
+  {
+    "revision": "fdaf8f9310667fe44167",
+    "url": "/example.checkout.v2/static/js/2.ba8f505f.chunk.js"
+  },
+  {
+    "revision": "54c6f82ce6bb59fa32d9",
+    "url": "/example.checkout.v2/static/js/main.9ab17585.chunk.js"
+  },
+  {
+    "revision": "933908964377c0edb90c",
+    "url": "/example.checkout.v2/static/js/runtime~main.2d8bcddb.js"
+  },
+  {
+    "revision": "3a5e09ccf0abc1502ec44fcabd274a5b",
+    "url": "/example.checkout.v2/static/media/arrow-icon.3a5e09cc.svg"
+  },
+  {
+    "revision": "d744b1f3c150e1e1cfc16ab893991e41",
+    "url": "/example.checkout.v2/static/media/cart-icon.d744b1f3.svg"
+  },
+  {
+    "revision": "763a8d77f190ed48786f300e4e8b503f",
+    "url": "/example.checkout.v2/static/media/letters-hero-big.763a8d77.svg"
+  },
+  {
+    "revision": "6607ab507f2602a8d1dbebb7dba113d5",
+    "url": "/example.checkout.v2/static/media/logo.6607ab50.svg"
+  },
+  {
+    "revision": "1655d83676faa2f8e6cccbec1e91df4d",
+    "url": "/example.checkout.v2/static/media/pair-shoes-small.1655d836.png"
+  },
+  {
+    "revision": "2d319ad9b754f578a813097fe231a65f",
+    "url": "/example.checkout.v2/static/media/remove-icon.2d319ad9.svg"
+  },
+  {
+    "revision": "a1632b7715729528c66bd5c5c29b3752",
+    "url": "/example.checkout.v2/static/media/shoes-hero.a1632b77.png"
+  }
+]);
