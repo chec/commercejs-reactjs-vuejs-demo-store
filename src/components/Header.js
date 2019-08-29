9259c18b9c372c2c71349efa5d99c0a697f45671
John Ray <john@trychec.com>
2019-08-29 12:39:46 -0400

    nest svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,12 +11,13 @@ import cartIcon from '../assets/cart-icon.svg';
 import pairShoes from '../assets/pair-shoes-small.png';
 
 class Header extends Component {
-  
   get config() {
     return {
       template: props => {
         return h('header', { attrs: { class: 'absolute w-100 ph5 pv5 flex flex-row justify-between mt3 z-1' }}, [
-          h('div', { attrs: { class: 'logoContainer dim pointer'}}, LogoSvg()),
+          h('a', { attrs: { href: "#", class: 'logoContainer dim pointer'}}, [
+            LogoSvg()
+          ]),
           h('div', { attrs: { class: 'flex'}}, [
             h('div', { attrs: { class: 'productFragmentContainer mw4'}}, [
               h('button', { attrs: { class: 'absolute right-0 medium-text f7 cherry bg-blossom ttu rotate-270 pv2 ph2 bg-white outline-0 dim pointer mr5 mt3', name: ''}}, [
