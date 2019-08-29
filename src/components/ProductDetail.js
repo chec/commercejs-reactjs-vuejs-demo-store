632ce9268a338874da0d39499429bf05ecf5c456
John Ray <john@trychec.com>
2019-08-29 12:42:13 -0400

    convert ProductDetail into Panel Web Component

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,44 +1,130 @@
+import { Component, h } from 'panel';
+
 import setUpShadowAndRender from '../utils/setUpShadowAndRender';
 import WithComponentState from '../utils/withComponentState';
 import parseProp from '../utils/unescapeAndParseAttribute';
 
 function renderLabel(props) {
-  return `
-    <div class="${props.classes || ''}">
-      <p class="medium-text ttu gray f6 tracked-mega-1 pb2">
-        ${props.labelTitle || '----'}
-      </p>
-      <p class="large-title-text f1 fw9 ttu pl3">
-        ${props.body || '----'}
-      </p>
-    </div>
-  `;
+  return h('div', { attrs: { class: props.classes || '' }}, [
+    h('p', { attrs: { class: 'medium-text ttu gray f6 tracked-mega-1 pb2'}}, [
+      props.labelTitle
+    ]),
+    h('p', { attrs: { class: 'large-title-text f1 fw9 ttu pl3'}}, [
+      props.body
+    ]),
+  ])
 }
 
 function renderSelect(props) {
-  const renderOptions = () => props.options.map(option => `
-    <option value="${option.value}" ${(props.selectedValue === option.value) ? 'selected="selected"' : ''}">
-      ${option.description}
-    </option>
-  `).join('')
+  const renderOptions = props.options.map(option => {
+    return h('option', { attrs: { value: option.value, selected: ((props.selectedValue === option.value) ? 'selected' : '')}}, [
+      option.description || ''
+    ])
+  })
 
-  return `
-    <div class="${props.classes || ''} relative">
-      ${renderLabel({
-        labelTitle: props.labelTitle,
-        body: props.labelBody,
-        classes: props.labelClasses
-      })}
-
-      <select class="absolute absolute--fill left-0 o-0 pointer w-100" value="${props.selectedValue || ''}" name="${props.selectName || ''}">
-        <option value="" disabled selected="selected">Choose a size</option>
-        ${renderOptions()}
-      </select>
-    </div>
-  `;
+  return h('div', { attrs: { class: `relative ${props.classes || ''}`}}, [
+    renderLabel({
+      labelTitle: props.labelTitle,
+      body: props.labelBody,
+      classes: props.labelClasses
+    }),
+    h('select', { on: { input: props.onInput }, attrs: { class: 'absolute absolute--fill left-0 o-0 pointer w-100'}}, [
+      h('option', { attrs: { value: '', disabled: true, selected: 'selected'}}, [
+        'Choose a size'
+      ]),
+      ...renderOptions
+    ])
+  ])
 }
 
-class ProductDetail extends WithComponentState() {
+class ProductDetail extends Component {
+
+  handleSizeSelect(e) {
+    this.update({
+      selectedSize: e.target.value
+    })
+  }
+
+  get config() {
+    return {
+      defaultState: {
+        selectedSize: ''
+      },
+
+      template: props => {
+        console.log('this is the props of ProductDetail', {...this})
+        this.product = props.products.length ? props.products[0] : null
+        const sizeOptions = this.product ?
+          this.product.variants[0].options.map(option => ({ value: option.id, description: option.name })) :
+          [];
+        const sizeOptionsById = this.product ?
+          this.product.variants[0].options.reduce((obj, currentOption) => {
+            obj[currentOption.id] = currentOption;
+            return obj;
+          }, {}) :
+          {};
+        return this.product ? h('div', { attrs: { class: 'productDetail w-100 pb5 ph4'}}, [
+          h('div', { attrs: { class: 'mw8 center ph2'}}, [
+            h('div', { attrs: { class: 'cf flex flex-row items-center'}}, [
+              h('div', { attrs: { class: 'fl flex flex-column flex-grow-1 items-center justify-center mw6 mt6'}}, [
+                h('p', { attrs: { class: 'large-title-text dark-gray w-100 ttl tc'}}, [
+                  this.product.name
+                ]),
+                h('p', { attrs: { class: 'medium-body-text gray w-90 tc'}}, [
+                  'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+                ]),
+                h('button', {
+                  on: {
+                    click: () => this.$panelRoot.addProductToCart(this.product.id)
+                  },
+                  class: {
+                    'dim': !!this.state.selectedSize,
+                    'o-30': !!!this.state.selectedSize
+                  },
+                  attrs: {
+                    disabled: !!!this.state.selectedSize,
+                    class: 'button button__add-to-cart white ttu bg-dark-gray tracked-mega-1 w-100 mv3'
+                  }}, [
+                  'add to cart'
+                ])
+              ]),
+              h('div', { attrs: { class: 'fl w-50 self-start relative'}}, [
+                h('img', { attrs: { src: this.product.media.source, width: '100%', height: 'auto'}}),
+                h('div', { attrs: { class: 'absolute absolute--fill flex justify-end items-end ml4' }}, [
+                  h('div', { attrs: { class: 'rotate-lift pr2'}}, [
+                    h('p', { attrs: { class: 'medium-text ttu gray f6 tracked-mega-1 pb2 b'}}, [
+                      'type'
+                    ]),
+                    h('p', { attrs: { class: 'large-title-text f1 fw9 ttu pl3'}}, [
+                      'brand'
+                    ])
+                  ])
+                ])
+              ])
+            ])
+          ]),
+          h('div', { attrs: { class: 'productDetail__info-container mw8 center flex flex-row flex-grow-1 pb4 mt4'}}, [
+            renderLabel({
+              labelTitle: 'price',
+              body: '$100.00 USD',
+              classes: 'mr5'
+            }),
+            renderSelect({
+              labelTitle: 'choose a size',
+              labelBody: sizeOptionsById[this.state.selectedSize] ? sizeOptionsById[this.state.selectedSize].name : 'choose a size',
+              selectName: 'sizeSelect',
+              selectedValue: this.state.selectedSize,
+              options: sizeOptions,
+              onInput: this.handleSizeSelect.bind(this)
+            })
+          ])
+        ]) : h('div', 'loading')
+      }
+    }
+  }
+}
+
+class _ProductDetail extends WithComponentState() {
 
   constructor() {
     super();
