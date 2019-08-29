cd87d7e7e61201f1a33a5e039c8ceb80252164f7
John Ray <john@trychec.com>
2019-08-29 15:23:56 -0400

    checkout remove-icon.svg from react.js branch, create vnode hyperscript definition of remove-icon.svg

---
+++ src/assets/RemoveIconSvg.js
@@ -0,0 +1,38 @@
+import { h } from 'panel';
+
+export default () => {
+  return h('svg', {
+    attrs: {
+      width: '100%',
+      height: '100%',
+      viewBox: '0 0 30 29',
+      fill: 'none'
+    }
+  }, [
+    h('ellipse', {
+      attrs: {
+        cx: "14.6088",
+        cy: "14.5",
+        rx: "14.6088",
+        ry: "14.5",
+        fill: "#545454"
+      }
+    }),
+    h('line', {
+      attrs: {
+        y1: "-0.5",
+        x2: "10.6728",
+        y2: "-0.5",
+        transform: "matrix(0.709745 -0.704458 0.709745 0.704458 11 18.5186)",
+        stroke: "#E37070"
+      }
+    }),
+    h('line', {
+      y1: "-0.5",
+      x2: "10.6728",
+      y2: "-0.5",
+      transform: "matrix(-0.709745 -0.704458 -0.709745 0.704458 18.575 18.5186)",
+      stroke: "#E37070"
+    })
+  ])
+}
