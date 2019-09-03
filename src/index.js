b9a2cc227ac3878740abf49f5b747f43211c6a1e
John Ray <john@trychec.com>
2019-09-02 20:48:22 -0400

    register ThankYou component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import LandingPage from './components/LandingPage';
 import ProductDetail from './components/ProductDetail';
 import CartCheckout from './components/CartCheckout';
+import ThankYou from './components/ThankYou';
 
 // TODO: create util function to DRY up customElementsRegistry.define use
 
@@ -15,3 +16,4 @@ customElements.define("x-header", Header);
 customElements.define("landing-page", LandingPage);
 customElements.define("product-detail", ProductDetail);
 customElements.define("cart-checkout", CartCheckout);
+customElements.define("thank-you", ThankYou);
