4c0ecca4286cfead068745e682aca4dc18d905b0
John Ray <john@trychec.com>
2019-09-05 16:42:56 -0400

    create Products container to render all products as ProductDetail components

---
+++ src/components/Products.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Loading from './Loading';
+import ProductDetail from './ProductDetail';
+
+class Products extends Component {
+  render() {
+    const allProducts = this.props.products.map((product, id) => {
+      return (
+        <ProductDetail
+            product={product}
+            addProductToCart={this.props.addProductToCart}
+          />
+      )
+    })
+    if (this.props.products.length) {
+      return (
+        <div className="w-100">
+          {allProducts.reverse()}
+        </div>
+      )
+    }
+    return (
+      <Loading />
+    )
+  }
+}
+
+export default Products
