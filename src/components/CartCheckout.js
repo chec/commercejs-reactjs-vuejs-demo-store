536b517e884127e2120252111e0976351d275558
John Ray <john@trychec.com>
2019-09-06 11:19:35 -0400

    make CartCheckout header's link direct to //products route not /white-shoe

--- src/components/CartCheckout.js
+++ src/components/CartCheckout.js
@@ -337,7 +337,7 @@ class CartCheckout extends Component {
     return (
       <div className="flex flex-grow-1 flex-column bg-tan-white w-100 pb4">
         <div className="flex justify-between mw9 w-100 items-center center pt4 ph4">
-          <Link to="/white-shoe" className="flex items-center medium-text f7 f6-ns tracked-mega ttu no-underline dark-gray dim">
+          <Link to="/products" className="flex items-center medium-text f7 f6-ns tracked-mega ttu no-underline dark-gray dim">
             <div className="arrowIconContainer fill-cherry pr4">
               <ArrowIcon />
             </div>
