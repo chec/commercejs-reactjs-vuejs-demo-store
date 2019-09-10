5f048976e88167a2af2b258518471185ad440723
John Ray <john@trychec.com>
2019-09-10 12:22:23 -0700

    add key prop to footprints div

--- src/components/Loaders.js
+++ src/components/Loaders.js
@@ -6,8 +6,8 @@ export function FootPrintsLoading(props) {
     <div className="FootPrintsLoader fixed absolute-fill w-100 h-100 bg-black-90 flex flex-column items-center justify-center z-max">
       <div className="FootPrintsLoader__prints-container">
         {
-         Array.from({length: '8'}).map(() => (
-           <div className="FootPrintsLoader__print footPrintContainer fill-cherry">
+         Array.from({length: '8'}).map((item, id) => (
+           <div className="FootPrintsLoader__print footPrintContainer fill-cherry" key={id}>
              <FootPrintSvg />
            </div>
          ))
