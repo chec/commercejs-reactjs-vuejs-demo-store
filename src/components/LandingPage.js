8b230ac062b04c52e6d337c90731b68a050b7bf1
John Ray <john@trychec.com>
2019-09-09 18:14:01 -0700

    adjust landingpage elements

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -14,11 +14,12 @@ function LandingPage(props) {
           <img src={shoesHero} alt="Pair of Shoes" width="100%" height="auto" />
         </div>
         <div className="absolute absolute--fill flex flex-column justify-center items-center nb7 ph3">
-          <p className="flex flex-shrink-1 flex-column hero-text f3 f2-m f1-l white tc pb5 lh-title tracked">
+          <Link to="/products" className="medium-text f7 cherry bg-blossom ttu pv3 ph3 bg-white outline-0 dim pointer self-center no-underline mt1">
+            shop products
+          </Link>
+          <p className="flex flex-shrink-1 flex-column hero-text f3 f2-m f1-l white tc pb5 pb0-l mt6-l lh-title tracked">
             LOREM IPSUM LOREM IPSUM
-            <Link to="/products" className="medium-text f7 cherry bg-blossom ttu pv3 ph3 bg-white outline-0 dim pointer self-center no-underline mt1">
-              shop products
-            </Link>
+
           </p>
         </div>
       </div>
