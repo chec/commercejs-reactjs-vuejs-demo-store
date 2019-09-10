3f51b6ece38e12898a5bea568dc4e2b16ea289cb
John Ray <john@trychec.com>
2019-09-09 18:15:20 -0700

    remove loading component, fallback to broader copy instead of being specific to 'orders'

--- src/components/Loaders.js
+++ src/components/Loaders.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import { ReactComponent as FootPrintSvg } from '../assets/footprint.svg';
 
-export function Loading() {
-  return (
-    <div className="absolute absolute--fill flex items-center justify-center">
-      <div className="footPrintContainer fill-cherry ">
-        <FootPrintSvg />
-      </div>
-      <p className="font-roboto ttu tracked-mega-1">
-        loading...
-      </p>
-    </div>
-  )
-}
-
 export function FootPrintsLoading(props) {
   return (
     <div className="FootPrintsLoader fixed absolute-fill w-100 h-100 bg-black-90 flex flex-column items-center justify-center z-max">
@@ -27,7 +14,7 @@ export function FootPrintsLoading(props) {
         }
       </div>
       <p className="absolute medium-text white f6 ttu">
-        { props.children || 'processing your order...'}
+        { props.children || 'loading...'}
       </p>
     </div>
   )
