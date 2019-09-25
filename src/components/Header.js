192ca2b961978e0caa45d59e25e6441d1ac7ba13
John Ray <john@trychec.com>
2019-09-25 18:32:48 -0400

    remove unused pairShoes, interpolate rootClasses to allow for hidding of header that won't cause an unmount, so that animation is not replayed when toggling visiblity of header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import {ReactComponent as Logo } from '../assets/logo.svg'
 import {ReactComponent as CartIcon} from '../assets/cart-icon.svg';
-import pairShoes from '../assets/pair-shoes-small.png'
 
 function Header(props) {
   const isThankYouPathname = props.location.pathname === '/thank-you';
@@ -11,13 +10,13 @@ function Header(props) {
   const productPageClass = props.location.pathname === '/products' ? 'headerContainer--products-page' : '';
 
   return (
-    <header className={`headerContainer absolute w-100 ph4 ph5-l pv5 pv5-l flex flex-row justify-between items-center z-1 ${productPageClass}`}>
+    <header className={`headerContainer absolute w-100 ph4 ph5-l pv5 pv5-l flex flex-row justify-between items-center z-1 ${productPageClass} ${props.rootClasses}`}>
       <Link to="/">
         <div className={`logoContainer dim pointer w-70 w-85-l ${currentFillColor}`}>
           <Logo />
         </div>
       </Link>
-      <div className="flex">
+      <div className={`flex ${props.playCartAnimation && 'scale-animation'}`}>
         <Link to="cart-checkout" className="flex flex-row items-center no-underline dim">
           <div className={`cartIconContainer pointer ${currentFillColor}`}>
             <CartIcon />
