c3ab8b9b1af221bcbbbe2c721f80820553a232df
John Ray <john@trychec.com>
2019-10-21 12:12:00 -0400

    use v2 Commerce.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
 import './styles/application.scss';
 import App from './App';
+import Commerce from '@chec.io/commerce';
 
-const commerce = new window.Commerce(process.env.REACT_APP_COMMERCEJS_PUBLIC_KEY, (process.env.NODE_ENV === 'development') ? true : false);
+const commerce = new Commerce(process.env.REACT_APP_COMMERCEJS_PUBLIC_KEY, (process.env.NODE_ENV === 'development') ? true : false);
 ReactDOM.render(<Router><App commerce={commerce}/></Router>, document.getElementById('root'));
