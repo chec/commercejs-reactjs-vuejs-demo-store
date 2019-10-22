8a3a1ad90f1bf444038004dc663e08764de8e9bc
John Ray <john@trychec.com>
2019-10-22 18:15:38 -0400

    implement scroll restorer component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
+import ScrollToTop from './lib/ScrollToTop';
 import './styles/application.scss';
 import App from './App';
 
-ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
+ReactDOM.render(<Router><ScrollToTop><App/></ScrollToTop></Router>, document.getElementById('root'));
