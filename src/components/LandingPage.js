7141a1cd62e7903b308f94c073812564e429b9ee
John Ray <john@trychec.com>
2019-10-23 17:35:11 -0400

    remove lorem ipsum hero text

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,9 +11,6 @@ function LandingPage(props) {
           <LettersHeroBg />
         </div>
         <div className="absolute absolute--fill flex flex-column items-center w-90 center mt6 justify-center">
-          <p className="absolute flex flex-shrink-1 flex-column hero-text f3 f2-m f1-l white tc lh-title tracked">
-            LOREM IPSUM LOREM IPSUM
-          </p>
           <img src={shoesHero} alt="Pair of Shoes" width="100%" height="auto" />
           <div className="relative absolute--fill flex flex-column justify-center items-center ph3">
             <Link to="/products" className="medium-text f7 cherry bg-blossom ttu pv3 ph3 bg-white outline-0 dim pointer self-center no-underline mt1">
