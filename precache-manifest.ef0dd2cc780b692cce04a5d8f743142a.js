645dfccd8774f0667b0e31018c5eb5dbf107877e
John Ray <john@trychec.com>
2019-10-25 18:14:48 -0400

    Updates

---
+++ precache-manifest.ef0dd2cc780b692cce04a5d8f743142a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48bad2e828245b05cc367c11ed90391b",
+    "url": "/example.checkout.v2/index.html"
+  },
+  {
+    "revision": "922e036dc8bfb9dd9610",
+    "url": "/example.checkout.v2/static/css/main.9569ac14.chunk.css"
+  },
+  {
+    "revision": "1062acc190df610c8bef",
+    "url": "/example.checkout.v2/static/js/2.0083ac95.chunk.js"
+  },
+  {
+    "revision": "922e036dc8bfb9dd9610",
+    "url": "/example.checkout.v2/static/js/main.3b48c534.chunk.js"
+  },
+  {
+    "revision": "933908964377c0edb90c",
+    "url": "/example.checkout.v2/static/js/runtime~main.2d8bcddb.js"
+  },
+  {
+    "revision": "3a5e09ccf0abc1502ec44fcabd274a5b",
+    "url": "/example.checkout.v2/static/media/arrow-icon.3a5e09cc.svg"
+  },
+  {
+    "revision": "d744b1f3c150e1e1cfc16ab893991e41",
+    "url": "/example.checkout.v2/static/media/cart-icon.d744b1f3.svg"
+  },
+  {
+    "revision": "cdc06d863f0acb0da664154115a60866",
+    "url": "/example.checkout.v2/static/media/down-arrow.cdc06d86.svg"
+  },
+  {
+    "revision": "8303cb28f915d5187838706836c3c482",
+    "url": "/example.checkout.v2/static/media/footprint.8303cb28.svg"
+  },
+  {
+    "revision": "763a8d77f190ed48786f300e4e8b503f",
+    "url": "/example.checkout.v2/static/media/letters-hero-big.763a8d77.svg"
+  },
+  {
+    "revision": "6607ab507f2602a8d1dbebb7dba113d5",
+    "url": "/example.checkout.v2/static/media/logo.6607ab50.svg"
+  },
+  {
+    "revision": "1655d83676faa2f8e6cccbec1e91df4d",
+    "url": "/example.checkout.v2/static/media/pair-shoes-small.1655d836.png"
+  },
+  {
+    "revision": "2d319ad9b754f578a813097fe231a65f",
+    "url": "/example.checkout.v2/static/media/remove-icon.2d319ad9.svg"
+  },
+  {
+    "revision": "ceb19562a44dbabf6cf943f7a08a8daa",
+    "url": "/example.checkout.v2/static/media/shoes-hero.ceb19562.png"
+  },
+  {
+    "revision": "2cacb41fdc238ae24bade4c0a0a79cea",
+    "url": "/example.checkout.v2/static/media/updated-sock-image.2cacb41f.png"
+  }
+]);
