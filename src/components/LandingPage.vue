0ea9f3421ec0c4b3fe525078ade0f44a90ac923c
John Ray <john@trychec.com>
2019-11-07 17:19:15 -0500

    update LandingPage to include changes from react.js branch LandingPage

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,19 +1,16 @@
 <template>
-  <div class="flex flex-grow-1 items-center bg-black pv5 pv4-m pv3-l">
-    <div class="relative w-100 mw9 center ph2">
+  <div class="flex flex-grow-1 items-center bg-black pv6">
+    <div class="relative w-100 mw9 center ph2 pb4">
       <div class="w-90 o-40 center pv2">
         <LettersHeroBgSvg />
       </div>
-      <div class="absolute absolute--fill flex flex-column items-center w-90 center mt5">
-        <img src="../assets/shoes-hero.png" alt="Shoes Hero" width="100%" height="auto" />
-      </div>
-      <div class="absolute absolute--fill flex flex-column justify-center items-center nb7 ph3">
-        <p class="flex flex-shrink-1 flex-column hero-text f3 f2-m f1-l white tr lh-title">
-          LOREM IPSUM LOREM IPSUM
-          <router-link to="/white-shoe" class="medium-text f7 cherry bg-blossom ttu pv3 ph3 bg-white outline-0 dim pointer mw4 self-end mr3 no-underline">
-            shop shoe
+      <div class="absolute absolute--fill flex flex-column items-center w-90 center mt6 justify-center">
+        <img src="../assets/shoes-hero.png" alt="Pair of Shoes" width="100%" height="auto" />
+        <div class="relative absolute--fill flex flex-column justify-center items-center ph3">
+          <router-link to="/white-shoe" class="medium-text f7 cherry bg-blossom ttu pv3 ph3 bg-white outline-0 dim pointer self-center no-underline mt1">
+            shop products
           </router-link>
-        </p>
+        </div>
       </div>
     </div>
   </div>
