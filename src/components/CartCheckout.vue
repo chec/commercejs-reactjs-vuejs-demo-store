3b95ce9c27d0f320c125b371876b605b4f330db5
John Ray <john@trychec.com>
2019-11-08 21:35:37 -0500

    update path for continue shopping link

--- src/components/CartCheckout.vue
+++ src/components/CartCheckout.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="cart" class="flex flex-grow-1 flex-column bg-tan-white w-100 pb4">
     <div class="flex justify-between mw9 w-100 items-center center pt4 ph4">
-      <router-link to="/white-shoe" class="flex items-center medium-text f6 tracked-mega ttu no-underline dark-gray dim">
+      <router-link to="/products" class="flex items-center medium-text f6 tracked-mega ttu no-underline dark-gray dim">
         <div class="arrowIconContainer fill-cherry pr4">
           <ArrowIconSvg />
         </div>
