0571d75e01ddd733bb8b83962e8d77876514ddc9
John Ray <john@trychec.com>
2019-12-06 14:34:35 -0500

    inject commercejs plugin globally

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
-const commerce = new window.Commerce(process.env.VUE_APP_COMMERCEJS_PUBLIC_KEY, (process.env.NODE_ENV === 'development') ? true : false);
+import commercejs from '@chec/commerce.js'
 Vue.config.productionTip = false
+
+
+// inject commercejs as a plugin, globally
+Vue.mixin({
+  beforeCreate() {
+    this.$commerce = commercejs
+  }
+})
 
 new Vue({
   router,
