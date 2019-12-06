e53e0dd10af8b23c6acf3b8a3e7f53ccacdcaa03
John Ray <john@trychec.com>
2019-12-06 13:02:27 -0500

    install npm commerce.js module, instantiate and export the instance

---
+++ src/configCommercejs.js
@@ -0,0 +1,16 @@
+import Commerce from '@chec/commerce.js';
+
+const commercejsConfig = {
+  axiosConfig: {
+    headers: {
+      "X-Chec-Agent": "commerce.js/v1"
+    }
+  },
+  url: process.env.VUE_APP_COMMERCEJS_API_URL || 'https://api.chec.io'
+}
+
+export default new Commerce(
+  process.env.VUE_APP_COMMERCEJS_PUBLIC_KEY,
+  process.env.NODE_ENV === 'development',
+  commercejsConfig
+);
