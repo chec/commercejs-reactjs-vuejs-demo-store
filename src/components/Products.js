283fdcebe6643eeab4b6412469df151c15d8fca2
John Ray <john@trychec.com>
2020-01-14 15:24:36 -0500

    change products initial state to null to only update it as a list (array) on successful request responst, other than that show loading if null

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,19 +4,27 @@ import ProductDetail from './ProductDetail';
 
 class Products extends Component {
   render() {
-    const allProducts = this.props.products.map((product, id) => {
-      return (
-        <ProductDetail
-            key={id}
-            product={product}
-            addProductToCart={this.props.addProductToCart}
-          />
-      )
-    })
-    if (this.props.products.length) {
-      return (
+    if (this.props.products) {
+      const allProducts = this.props.products.map((product, id) => {
+        return (
+          <ProductDetail
+              key={id}
+              product={product}
+              addProductToCart={this.props.addProductToCart}
+            />
+        )
+      })
+      return this.props.products.length ?
+      (
         <div className="w-100">
           {allProducts.reverse()}
+        </div>
+      ) :
+      (
+        <div className="flex self-stretch w-100 justify-center items-center">
+          <p className="medium-text f6 tracked-mega ttu dark-gray tracked-mega">
+            There are currently no items for sale.
+          </p>
         </div>
       )
     }
