15548ce6c26f7c651319d0197df51c6d4325e718
John Ray <john@trychec.com>
2020-01-22 13:04:25 -0800

    throw error when in dev mode and missing chec public key for commerce.js to function

--- src/configCommercejs.js
+++ src/configCommercejs.js
@@ -1,5 +1,10 @@
 import Commerce from '@chec/commerce.js';
 
+const commercejsPublicKey = process.env.VUE_APP_COMMERCEJS_PUBLIC_KEY;
+const isDevMode = process.env.NODE_ENV === 'development';
+if (isDevMode && !commercejsPublicKey) {
+  throw Error('You must provide your public API key as an environment variable named VUE_APP_COMMERCEJS_PUBLIC_KEY for commerce.js to work. Your public key is available within your Chec dashboard.')
+}
 export default new Commerce(
   process.env.VUE_APP_COMMERCEJS_PUBLIC_KEY,
   process.env.NODE_ENV === 'development'
