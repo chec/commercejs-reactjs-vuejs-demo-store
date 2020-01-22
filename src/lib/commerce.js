24213c6a84474b4d94a5f448a4dbd00dff8f7fab
John Ray <john@trychec.com>
2020-01-22 15:29:06 -0800

    throw error when in dev mode and missing chec public key for commerce js to function, update env variables

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,6 +1,11 @@
 import Commerce from '@chec/commerce.js';
 
+const commercejsPublicKey = process.env.REACT_APP_COMMERCEJS_PUBLIC_KEY;
+const isDevMode = process.env.NODE_ENV === 'development';
+if (isDevMode && !commercejsPublicKey) {
+  throw Error('You must provide your public API key as an environment variable named REACT_APP_COMMERCEJS_PUBLIC_KEY for Commerce.js to work. Your public key is available within your Chec dashboard, or can be obtained with the Chec CLI via "whoami')
+}
 export default new Commerce(
-  process.env.REACT_APP_COMMERCEJS_PUBLIC_KEY,
-  process.env.NODE_ENV === 'development'
+  commercejsPublicKey,
+  isDevMode
 );
